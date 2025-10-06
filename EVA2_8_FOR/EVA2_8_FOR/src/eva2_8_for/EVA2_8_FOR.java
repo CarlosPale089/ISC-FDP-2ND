d5aa64129edd1afae8277a625c0cdbc62bfd0ea0
CarlosPale089 <L25550593@chihuahua2.tecnm.mx>
2025-10-06 11:38:00 -0600

    Add files via upload

---
+++ EVA2_8_FOR/EVA2_8_FOR/src/eva2_8_for/EVA2_8_FOR.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_8_for;
+
+/**
+ *
+ * @author unipa
+ */
+public class EVA2_8_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //3 parametros
+        //variable y valor de inicio
+        //hasta donde llegas
+        //incremento o decremento
+        // i++ significa sumarle un valor de la i
+        for(int i = 1; i<= 10; i++){
+            System.out.println("Hola mundo");
+           
+       
+       } for(int i = 10;i>= 1; i--){
+            System.out.println(i);}
+       
+    }
+    
+}
