1846276ca1ef55c715607c164843d9259caab85c
CarlosPale089 <L25550593@chihuahua2.tecnm.mx>
2025-10-13 10:46:00 -0600

    Add files via upload

---
+++ EVA2_9_FOR/EVA2_9_FOR/src/eva2_9_for/EVA2_9_FOR.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_9_for;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author unipa
+ */
+public class EVA2_9_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner input = new Scanner (System.in);
+       int rep;
+       String mensaje;
+        System.out.println("Mensaje a repetir: ");
+       mensaje = input.nextLine();
+        System.out.println("Cantidad de repeticiones: ");
+        rep = input.nextInt();
+        for(int i = 1; i <= rep; i++){
+            System.out.println(mensaje);
+        }
+    }
+    
+}
