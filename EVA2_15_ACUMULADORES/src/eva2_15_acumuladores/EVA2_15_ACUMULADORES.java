e909f591896e2372d4a78923fa18451e31bbc543
CarlosPale089 <L25550593@chihuahua2.tecnm.mx>
2025-11-02 20:14:26 -0600

    Add files via upload

---
+++ EVA2_15_ACUMULADORES/src/eva2_15_acumuladores/EVA2_15_ACUMULADORES.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_15_acumuladores;
+
+/**
+ *
+ * @author unipa
+ */
+public class EVA2_15_ACUMULADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //ACUMULADOR:
+        //++ -- += -= *= /=
+
+        int cuenta = 0;
+        //ASIGNACION ---> =
+        cuenta = cuenta + 100; //ACUMULADOR
+        System.out.println(cuenta);
+        cuenta = cuenta + 1000;
+        System.out.println(cuenta);
+        cuenta = cuenta - 800;
+        System.out.println(cuenta);
+        cuenta += 2000;
+        System.out.println(cuenta);
+        
+
+    }
+
+}
