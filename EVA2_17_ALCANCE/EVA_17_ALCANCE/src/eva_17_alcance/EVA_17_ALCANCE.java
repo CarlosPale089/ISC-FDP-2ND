ed811c845c4f2479ad4e9e247318801ff5de82d0
CarlosPale089 <L25550593@chihuahua2.tecnm.mx>
2025-11-02 20:17:03 -0600

    Add files via upload

---
+++ EVA2_17_ALCANCE/EVA_17_ALCANCE/src/eva_17_alcance/EVA_17_ALCANCE.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_17_alcance;
+
+/**
+ *
+ * @author unipa
+ */
+public class EVA_17_ALCANCE {
+
+ 
+    public static void main(String[] args) {
+        int i = 5;
+        {System.out.println(i);
+            int valor =100;
+            System.out.println(valor);}
+        System.out.println(valor);
+    }
+    
+}
