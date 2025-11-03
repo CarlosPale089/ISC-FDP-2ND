3e0a129bc8029f71dde6fbd030b88fb8a15f9693
CarlosPale089 <L25550593@chihuahua2.tecnm.mx>
2025-11-02 20:17:22 -0600

    Add files via upload

---
+++ EVA2_18_WHILE/EVA2_18_WHILE/src/eva2_18_while/EVA2_18_WHILE.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_18_while;
+
+/**
+ *
+ * @author unipa
+ */
+public class EVA2_18_WHILE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 1;
+     while(i<=10){ // se va a repetir si la condicion es verdadera
+            System.out.println("Carlos castillo");
+     i++;}
+     
+        System.out.println("CICLO DO WHILE-----------");
+     i = 1;
+     do {
+         System.out.println("");
+         i++;
+     } while(i <=10);
+     }
+     
+    }
+    
+
