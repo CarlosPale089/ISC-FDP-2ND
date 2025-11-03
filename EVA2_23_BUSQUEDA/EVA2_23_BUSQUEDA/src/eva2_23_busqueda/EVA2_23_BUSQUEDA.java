d3496eaf734abd8ad34af8977813661ef33f6975
CarlosPale089 <L25550593@chihuahua2.tecnm.mx>
2025-11-02 20:19:05 -0600

    Add files via upload

---
+++ EVA2_23_BUSQUEDA/EVA2_23_BUSQUEDA/src/eva2_23_busqueda/EVA2_23_BUSQUEDA.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_23_busqueda;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author unipa
+ */
+public class EVA2_23_BUSQUEDA {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        int valor, posi = -1; // -1 significa que no lo encontramos
+        int datos[] = new int [10];
+        
+        for (int i = 0; i < datos.length; i++) {
+          datos[i] = (int)(Math.random() * 100);
+        }
+         for (int i = 0; i < datos.length; i++) {
+             System.out.print("[" + datos [i] + "]");
+         }
+         System.out.println("");
+         System.out.println("Valor a buscar: ");
+         valor = input.nextInt();
+         //Busqueda lineal
+         for (int i = 0; i < datos.length; i++) {
+             if(valor == datos[i]){// Lo encontramos
+                 posi = i;
+                 break;
+             }
+         }
+         System.out.println("Posición: " + posi);
+    }
+    
+}
